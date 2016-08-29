1a268712ccee4bd018c237f85096e42464d0e0ba
Kimberly Pennington <kimberlythegeek@gmail.com>
2016-08-29 17:26:47 -0600

    added contact submit enable script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,4 +14,25 @@ $(document).ready(function(){
       scrollTop: $('#contact').position().top
     }, 300);
   });
+
+  $("input, textarea").on("keyup", function(){
+
+    if($(this).val() != '') {
+      $('.' + this.id).addClass('has-success');
+      $('.' + this.id).addClass('glyphicon-ok');
+
+      $('.' + this.id).removeClass('has-error');
+      $('.' + this.id).removeClass('glyphicon-remove');
+    } else {
+      $('.' + this.id).addClass('has-error');
+      $('.' + this.id).addClass('glyphicon-remove');
+
+      $('.' + this.id).removeClass('has-success');
+      $('.' + this.id).removeClass('glyphicon-ok');
+    }
+
+    if ($("textarea").val() != "" && $("#form-name").val() != "" && $("#form-phone").val() != "" && $("#form-email").val() != "") {
+      $("#submit-form").removeAttr("disabled");
+    }
+  });
 });
