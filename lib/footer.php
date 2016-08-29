0e59894b0ebfc3fc82d6ceb1bc2745666e38fa97
Kimberly Pennington <kimberlythegeek@gmail.com>
2016-08-29 17:16:31 -0600

    minor style changes

--- lib/footer.php
+++ lib/footer.php
@@ -2,6 +2,23 @@
   </body>
 
   <footer>
-
+    <div class="row footer text-center">
+      <div class="col-lg-6 col-lg-offset-3 site-info">
+        <p>Website Design by <a href="http://kimberly.codes">Kimberly Pennington</a> of <a href="http://www.acodeabove.com">A Code Above</a></p>
+      </div>
+      <div class="col-lg-3 text-right site-seal">
+        <span id="siteseal">
+          <script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=RKfrNrDOAkZHuSscM5xzEb0NLsA4qhkhO2fILxEjG0p9sQmygu9ALiQ2IG1t"></script>
+        </span>
+      </div>
+    </div><!-- .row .footer -->
+    <div class="row footer text-left links">
+      <div class="col-lg-12">
+        <ul class="list-unstyled list-inline">
+          <li><a href="http://localhost/acodeabove/terms">Terms & Conditions</a></li>
+          <li><a href="http://localhost/acodeabove/privacy-policy.php">Privacy Policy</a></li>
+        </ul>
+      </div>
+    </div>
   </footer>
 </html>
