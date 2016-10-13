436ffb6855537312f4dad41711ce63a3ea66e41d
Kimberly Pennington <kimberlythegeek@gmail.com>
2016-10-13 11:08:31 -0600

    fixed links on privacy policy page

--- privacy-policy.php
+++ privacy-policy.php
@@ -1,4 +1,84 @@
-<?php include 'lib/header.php' ?>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Baloo+Paaji|Open+Sans:300,400,800" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="assets/js/scripts.js"></script>
+
+
+    <link rel="stylesheet" type="text/css" href="assets/css/featherlight/featherlight.css">
+    <script src="assets/js/featherlight/featherlight.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets/css/featherlight/featherlight.gallery.css">
+    <script src="assets/js/featherlight/featherlight.gallery.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+    <title>Durango Web Design | A Code Above | Web Design & Development</title>
+    <meta name="web design and development" content="A Code Above | Professional, Aesthetic and Efficient Web Design & Development | A Colorado Proud Company | Based in Durango, Colorado" />
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> <!--320-->
+  </head>
+
+  <header>
+    <?php include_once 'analytics.php'; ?>
+  </header>
+
+  <body>
+    <div id="site-description" style="display:none;">Durango Web Design. Professional Web Development in Durango, Colorado. Mobile Web Design.</div>
+    <div class="site">
+
+      <!-- Small modal -->
+      <button type="button" class="btn btn-primary contact-btn" data-toggle="modal" data-target=".contact-modal">Contact Us!</button>
+      <?php include 'modal.php' ?>
+      <div class="site-header">
+        <div class="site-branding">
+          <div class="logo">
+            <a href="http://acodeabove.com"><img src="assets/img/logo_sm.png" /></a>
+          </div><!-- .logo -->
+
+          <div class="site-title">
+            <span><a href="http://acodeabove.com">A Code Above</a></span>
+          </div><!-- .site-title -->
+        </div><!-- .branding -->
+
+        <nav class="navbar navbar-default">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li><a class="home" href="http://acodeabove.com">Home</a></li>
+                <li><a class="company" href="http://acodeabove.com/#company"><span class="narrow">Our </span>Company</a></li>
+                <li><a class="portfolio" href="http://acodeabove.com/#portfolio">Portfolio</a></li>
+                <li><a class="services" href="http://acodeabove.com/#services">Services</a></li>
+                <li><a class="contact" href=""  data-toggle="modal" data-target=".contact-modal">Contact<span class="narrow"> Us</span></a></li>
+                <!-- <li><a class="blog" href="http://acodeabove.com/blog">Blog</a></li> -->
+              </ul>
+            </div><!-- .navbar-collapse -->
+        </nav>
+
+        </div><!-- .navigation -->
+      </div> <!-- .site-header -->
+
+
+      <div id="page">
+
+
+
 <div class="privacy">
   <div class="">
     <h1 class="text-center"><a href="http://www.acodeabove.com">A Code Above</a> Privacy Policy</h1>
