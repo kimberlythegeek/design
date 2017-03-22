16c2ab121b477c709d832bfe5cb08b50023c3331
Kimberly Pennington <email@kimberly.codes>
2017-03-22 10:06:58 -0600

    optimizing for speed

--- lib/footer.php
+++ lib/footer.php
@@ -3,7 +3,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js" async defer></script>
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" async defer></script>
-    <script src="assets/js/scripts.js" async defer></script>
+    <script src="assets/js/scripts.min.js" async defer></script>
   </body>
 
   <footer>
